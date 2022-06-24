b567631fa29667ef615e331f624a80baf2e0699b
Barkley <barkley.santo@gmail.com>
2022-06-24 17:40:04 +0300

    new stylesheet object for 2nd btn

--- App.js
+++ App.js
@@ -8,19 +8,28 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>hi!</Text>
+        <Text style={styles.first}>hi!</Text>
       </View>
-      <Text>hi again</Text>
-      <Button title="Tap me!" onPress={alertHi} />
+      <Text style={styles.first}>hi again</Text>
+      <Button title="Tap me!" onPress={alertHi} style={styles.button} />
     </View>
   )
 }
 
+// best to do it this way - RNs Stylesheet object provides validations + errors
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  first: {
+    backgroundColor: "pink",
+    borderWidth: 4,
+    borderColor: "blue",
+    padding: 20,
+    borderRadius: 6,
+    margin: 5,
   },
 })
