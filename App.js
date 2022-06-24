31fa8f35a39d2c12789f74c5800844007cd98afe
Barkley <barkley.santo@gmail.com>
2022-06-24 14:00:29 +0300

    add button component with onPress alert

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native"
 
 export default function App() {
+  function alertHi() {
+    alert("hellooo")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>hi!</Text>
+      </View>
+      <Text>hi again</Text>
+      <Button title="Tap me!" onPress={alertHi} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+})
