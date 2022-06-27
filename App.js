de8c3239bbfb26bc34e15abaaf779c95dfeba48f
Barkley <barkley.santo@gmail.com>
2022-06-27 15:42:53 +0300

    flexbox for main container- split top and bottom half of app

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
         <TextInput placeholder="Your course goal" style={styles.textInput} />
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of Goals</Text>
       </View>
     </View>
@@ -17,17 +17,28 @@ export default function App() {
 // best to do it this way - RNs Stylesheet object provides validations + errors
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1,
   },
   inputContainer: {
     display: "flex",
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
-    // backgroundColor: "lightblue",
+    alignItems: "center",
+    marginBottom: 25,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "black",
-    width: "80%",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 4,
   },
 })
