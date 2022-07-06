ee47cf325c76b85e3279ae5f6682bbf882d4b391
Barkley <barkley.santo@gmail.com>
2022-07-06 17:21:27 +0300

    add image to input component - add top level background color in app.json - padding to text area - change button colors

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,4 +1,4 @@
-import { View, TextInput, Button, StyleSheet, Modal } from "react-native"
+import { View, TextInput, Button, StyleSheet, Modal, Image } from "react-native"
 import { useState } from "react"
 
 function GoalInput(props) {
@@ -17,6 +17,10 @@ function GoalInput(props) {
   return (
     <Modal visible={props.visible} animationType="slide">
       <View style={styles.inputContainer}>
+        <Image
+          source={require("../assets/images/goal.png")}
+          style={styles.image}
+        />
         <TextInput
           placeholder="Course goals my dude"
           style={styles.textInput}
@@ -26,10 +30,14 @@ function GoalInput(props) {
 
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title="Add Goal" onPress={addGoalHandler} />
+            <Button
+              title="Cancel"
+              onPress={props.cancelModal}
+              color="#f31282"
+            />
           </View>
           <View style={styles.button}>
-            <Button title="Cancel" onPress={props.cancelModal} />
+            <Button title="Add Goal" onPress={addGoalHandler} color="#b180f0" />
           </View>
         </View>
       </View>
@@ -45,16 +53,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 25,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
+    // marginBottom: 25,
+    // borderBottomWidth: 1,
+    // borderBottomColor: "#ccc",
     padding: 16,
+    backgroundColor: "#311b6b",
   },
   textInput: {
     borderWidth: 1,
-    borderColor: "black",
+    borderColor: "#e4d0ff",
+    backgroundColor: "#e4d0ff",
     width: "100%",
-    padding: 8,
+    padding: 16,
+    color: "#120438",
+    borderRadius: 6,
   },
   buttonContainer: {
     marginTop: 16,
@@ -63,5 +75,10 @@ const styles = StyleSheet.create({
   button: {
     width: "30%",
     marginHorizontal: 8,
+  },
+  image: {
+    width: 75,
+    height: 75,
+    margin: 20,
   },
 })
